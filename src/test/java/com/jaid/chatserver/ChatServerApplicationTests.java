3343d6c96d8c5b15bc03e455f8d852c589c6591b
DARathnasiri <dilankar.17@cse.mrt.ac.lk>
2022-03-07 00:10:45 +0530

    initialize the project

---
+++ src/test/java/com/jaid/chatserver/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaid.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
